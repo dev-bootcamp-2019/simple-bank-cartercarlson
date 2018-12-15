4846e5b5e81486be4cd3674439df799a2bbc1067
Carter Carlson <cartercarlson94@gmail.com>
2018-12-15 13:52:14 -0600

    Completed homework, still need to test

--- contracts/SimpleBank.sol
+++ contracts/SimpleBank.sol
@@ -4,20 +4,20 @@ contract SimpleBank {
     //
     // State variables
     //
-    
+
     /* Fill in the keyword. Hint: We want to protect our users balance from other contracts*/
     mapping (address => uint) internal balances;
-    
+
     /* Fill in the keyword. We want to create a getter function and allow contracts to be able to see if a user is enrolled.  */
     mapping (address => bool) public enrolled;
 
     /* Let's make sure everyone knows who owns the bank. Use the appropriate keyword for this*/
     address public owner;
-    
+
     //
     // Events - publicize actions to external listeners
     //
-    
+
     /* Add an argument for this event, an accountAddress */
     event LogEnrolled(address accountAddress);
 
@@ -33,45 +33,71 @@ contract SimpleBank {
     //
 
     /* Use the appropriate global variable to get the sender of the transaction */
-    constructor() {
+    constructor()
+		public
+	{
         /* Set the owner to the creator of this contract */
-
+		owner = msg.sender;
     }
 
     /// @notice Get balance
     /// @return The balance of the user
     // A SPECIAL KEYWORD prevents function from editing state variables;
     // allows function to run locally/off blockchain
-    function balance() public returns (uint) {
+    function balance()
+		public
+		view
+		returns (uint)
+	{
         /* Get the balance of the sender of this transaction */
+		return balances[msg.sender];
     }
 
     /// @notice Enroll a customer with the bank
     /// @return The users enrolled status
     // Emit the appropriate event
-    function enroll() public returns (bool){
+    function enroll()
+		public
+		returns (bool)
+	{
+		LogEnrolled(msg.sender);
+		return true;
     }
 
     /// @notice Deposit ether into bank
     /// @return The balance of the user after the deposit is made
     // Add the appropriate keyword so that this function can receive ether
     // Use the appropriate global variables to get the transaction sender and value
-    // Emit the appropriate event    
-    function deposit() public returns (uint) {
+    // Emit the appropriate event
+    function deposit()
+		public
+		returns (uint)
+	{
         /* Add the amount to the user's balance, call the event associated with a deposit,
           then return the balance of the user */
+		  balances[msg.sender] += msg.value;
+		  LogDepositMade(msg.sender, msg.value);
+		  return balances[msg.sender];
     }
 
     /// @notice Withdraw ether from bank
     /// @dev This does not return any excess ether sent to it
     /// @param withdrawAmount amount you want to withdraw
     /// @return The balance remaining for the user
-    // Emit the appropriate event    
-    function withdraw(uint withdrawAmount) public returns (uint) {
+    // Emit the appropriate event
+    function withdraw(uint withdrawAmount)
+		public
+		returns (uint)
+	{
         /* If the sender's balance is at least the amount they want to withdraw,
            Subtract the amount from the sender's balance, and try to send that amount of ether
-           to the user attempting to withdraw. 
+           to the user attempting to withdraw.
            return the user's balance.*/
+	   	if (balances[msg.sender] >= withdrawAmount) {
+			balances[msg.sender] -= withdrawAmount;
+			msg.sender.send(withdrawAmount);	
+		}
+		return balances[msg.sender];
     }
 
     // Fallback function - Called if other functions don't match call or
